16d86d2fb4c7370e1ffbe7f049cee08098b0e787
Morad17 <morad123@hotmail.co.uk>
2024-01-05 14:24:22 +0900

    up

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 
+dotenv.config()
 const app = express()
 
 
-mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connect(process.env.REACT_APP_MONGODB_URI)
+console.log(process.env.REACT_APP_MONGODB_URI);
 
 app.listen( 5000, console.log("server is running"))
